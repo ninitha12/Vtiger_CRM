84fe47bedaec6b1f83afb99384a03c0c45177938
GEETHA <GEETHA@DESKTOP-8KTIUQD>
2025-12-26 17:22:44 +0530

    Adding the sysout statements

--- src/test/java/createcampaign/Campaign.java
+++ src/test/java/createcampaign/Campaign.java
@@ -3,6 +3,7 @@ package createcampaign;
 public class Campaign {
 	public static void main(String[] args) {
 		System.out.println("this is create campaign method");
+		System.out.println("Geetha is Adding this ");
 	}
 
 }
