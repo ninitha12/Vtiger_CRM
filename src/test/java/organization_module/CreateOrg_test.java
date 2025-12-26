3992589e78c8a6ac9df4700a896890a01a10c89e
Sharon <Sharon@LAPTOP-1DLOQGU3>
2025-12-26 15:38:08 +0530

    edited

--- src/test/java/organization_module/CreateOrg_test.java
+++ src/test/java/organization_module/CreateOrg_test.java
@@ -1,6 +1,10 @@
 package organization_module;
 
 public class CreateOrg_test {
-  System.out.println("Hiii Ninitha I am Geetha")
+	public static void main(String[] args) {
+		
+	
+	System.out.println("hi"); 
+	}
 
 }
